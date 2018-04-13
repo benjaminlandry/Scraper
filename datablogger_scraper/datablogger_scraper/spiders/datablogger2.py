515ab1e91596363966f07d76a8b8568426aca890
benjaminlandry <benjamin.landry@mail.mcgill.ca>
2018-04-13 17:57:14 -0400

    working on tree structure part 3

--- datablogger_scraper/datablogger_scraper/spiders/datablogger2.py
+++ datablogger_scraper/datablogger_scraper/spiders/datablogger2.py
@@ -16,7 +16,7 @@ import time
 
 class DatabloggerSpider(CrawlSpider):
     # The name of the spider
-    name = "jobs"
+    name = "jobsTest"
 
     # The domains that are allowed (links to other domains are skipped)
     allowed_domains = ['142.133.174.148']
@@ -29,12 +29,9 @@ class DatabloggerSpider(CrawlSpider):
     # Method which starts the requests by visiting all URLs specified in start_urls
     def start_requests(self):
         self.tree = Tree()
-        self.tree.create_node(self.start_urls[0], self.start_urls[0])
-        self.parent_link = self.start_urls[0]
-        print("PARENT_LINK_START", self.parent_link)
-
-        for url in self.start_urls:
-            yield scrapy.Request(url, callback=self.parse, dont_filter=True)
+        parent_link = self.start_urls[0].replace("http://142.133.174.148:8888/", "")
+        self.tree.create_node(parent_link, parent_link)
+        yield scrapy.Request(self.start_urls[0], callback=self.parse, dont_filter=True)
         
     # Method for parsing items
     def parse(self, response):
@@ -42,6 +39,10 @@ class DatabloggerSpider(CrawlSpider):
             self.start_requests()
             self.method_index = False 
         
+        parent_link = response.url
+        parent_link = parent_link.replace("http://142.133.174.148:8888/", "")
+        print(parent_link)
+
         # Fetch the html from the given url
         with urllib.request.urlopen(response.url) as response:
             current_page = response.read().decode('utf-8')
@@ -51,29 +52,25 @@ class DatabloggerSpider(CrawlSpider):
             # Extract URL from the html, using xpath
             links = regex_response.xpath('//div[@class="work_area_content"]/a/@href')
 
-            ## Store URLs in a tree or dictionary-list data structure
+            ## Store URLs in a tree or dictionary-list data structure | links stores all the children
             # set parent of a node e.g.(link) to the variable parent_link
             for link in links: ### HERE ###
                 try:
-                    print('PARENT_LINK_A', self.parent_link)
-                    self.parent_link = self.tree.parent(link)
-                    print('PARENT_LINK_B', self.parent_link)
+                    print('PARENT_LINK_A', parent_link)  ### HERE produces correct parent_link value
+                    # Turn the relative url to an absolute url
+                    absolute_url = "".join('http://142.133.174.148:8888/' + link)   
+                    # TODO: create tree structure with each element of link
+                    data = [link, absolute_url]
+                    print(self.tree.create_node(link, link, parent=parent_link, data=data)) ### HERE does NOT produces correct parent_link value | issue is create_node function.
+                    request = scrapy.Request(absolute_url, callback=self.parse, dont_filter=False)
+                    # Callback Parse function if links variable contain urls
+                    logging.warning('YIELD')
+                    #yield {'data': data, 'link': link, 'parent_link': self.parent_link}
+                    yield request
                 except:
-                    print('PARENT_LINK_C', self.parent_link)
+                    print('PARENT_LINK_C', parent_link)
                     traceback.print_exc()
-
-                # Turn the relative url to an absolute url
-                absolute_url = "".join('http://142.133.174.148:8888/' + link)   
-                # TODO: create tree structure with each element of link
-                data = [link, absolute_url]
-                print(self.tree.create_node(link, link, parent=self.parent_link, data=data)) 
-                request = scrapy.Request(absolute_url, callback=self.parse, dont_filter=False)
-                # Callback Parse function if links variable contain urls
-                logging.warning('YIELD')
-                #yield {'data': data, 'link': link, 'parent_link': self.parent_link}
-                yield request
-
-            time.sleep(1)
+                time.sleep(1)
 
             #self.tree.show()
             #print(self.tree.to_json(with_data=True))
--- datablogger_scraper/datablogger_scraper/tree1.py
+++ datablogger_scraper/datablogger_scraper/tree1.py
@@ -1,24 +1,24 @@
 from treelib import Node, Tree
 tree = Tree()
-# tree.create_node("Harry", "harry")  # root node
-# tree.create_node("Jane", "jane", parent="harry")
-# tree.create_node("Bill", "bill", parent="harry")
-# tree.create_node("Diane", "diane", parent="jane")
-# tree.show()
+tree.create_node("Harry", "Harry")  # root node
+tree.create_node("Jane", "Jane", parent="Harry")
+tree.create_node("Bill", "Bill", parent="Harry")
+tree.create_node("Diane", "Diane", parent="Jane")
+tree.show()
 
 #params: start_urls | links | link | absolute_url
 
-tree.create_node(start_urls)
+# tree.create_node(start_urls)
 
 
-parent_link = response.meta.link
+# parent_link = response.meta.link
 
-for link in links:
-    absolute_url = absolute_url
-    link = link
+# for link in links:
+#     absolute_url = absolute_url
+#     link = link
 
-    data = [link, absolute_url]
-    if(link != null)
-        tree.create_node(link, link, parent_link, data)
+#     data = [link, absolute_url]
+#     if(link != null)
+#         tree.create_node(link, link, parent_link, data)
 
-        requests(absolute_url, meta = link)
+#         requests(absolute_url, meta = link)
