387217998e2f48d7c7058f99a7d043bbd4491434
benjaminlandry <benjamin.landry@mail.mcgill.ca>
2018-04-20 14:53:14 -0400

    trial2spider file crawler now working | to be reviewed

--- datablogger_scraper/datablogger_scraper/spiders/trial2spider.py
+++ datablogger_scraper/datablogger_scraper/spiders/trial2spider.py
@@ -24,7 +24,7 @@ class DatabloggerSpider(CrawlSpider):
     allowed_domains = ['142.133.174.148']
     
     # The URLs to start with
-    start_urls = ['http://142.133.174.148:8888/AfgSutTestSuites']
+    start_urls = ['http://142.133.174.148:8888/AfgOpenIdConnectTestSuites']
     #start_urls = ['http://142.133.174.148:8888/TestCases']
 
     method_index = True
@@ -36,14 +36,16 @@ class DatabloggerSpider(CrawlSpider):
             self.root_url = self.start_urls[0]
             self.root_link = self.start_urls[0].replace("http://142.133.174.148:8888/", "")
             self.root = AnyNode(id=self.root_link)
-            print("THE START")
             self.method_index = False 
         
         ## Replace current_URL to Parent_URL if first or non-first run
         parent_url = self.root_url
+        parent_link = self.root_link
+        parent = self.root
         if (response.url != self.root_url):
             parent_url = response.url
-            parent = parent_url.replace("http://142.133.174.148:8888/", "")
+            parent_link = parent_url.replace("http://142.133.174.148:8888/", "")
+            parent = response.meta['parent']
             print("I am your parent", parent)
         
         ## GET and Filter out links from webpage
@@ -61,11 +63,13 @@ class DatabloggerSpider(CrawlSpider):
         ## Create multiple nodes for current_parent
         for link in links:
             ## Create a node for current_parent
+            current_node = AnyNode(id=link, parent=parent)
 
-
-            ## Recursively call parse function passing current_url
+            ## Recursively call parse function passing current_url and current_node (format url & NodeMixin, respectively)
             current_url = "".join('http://142.133.174.148:8888/' + link)
-            yield scrapy.Request(current_url, callback=self.parse)
+            request =  scrapy.Request(current_url, callback=self.parse)
+            request.meta['parent'] = current_node
+            yield request
 
         ## Print root-tree that displays a node's relationship between its parent and its potential children. Each node is in NodeMixin format.
         print("RENDER:", RenderTree(self.root))
